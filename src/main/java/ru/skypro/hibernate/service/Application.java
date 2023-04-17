bc4b2a09d199f66dbd9c5f19b328febadf9da608
admin <mmd83@63.ru>
2023-04-17 19:42:11 +0400

    EmoloyeeDAO

--- src/main/java/ru/skypro/hibernate/service/Application.java
+++ src/main/java/ru/skypro/hibernate/service/Application.java
@@ -1,15 +1,12 @@
 package ru.skypro.hibernate.service;
 
-import ru.skypro.hibernate.model.City;
-import ru.skypro.hibernate.model.Employee;
-
 public class Application {
     public static void main(String[] args) {
         EmployeeDAO employeeDao = new EmployeeDaoImpl();
 
         System.out.println(((EmployeeDaoImpl) employeeDao).getAllEmployee());
 
-        employeeDao.addEmployee(new Employee("Ivan", "Ivanov", "male", 59, new City("Moscow")));
+//        employeeDao.addEmployee(new Employee("Ivan", "Ivanov", "male", 59, new City("Moscow")));
 
 
 
