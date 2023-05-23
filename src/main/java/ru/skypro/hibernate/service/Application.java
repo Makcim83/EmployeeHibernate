0e4cd038a32613561e4b3f3b0b6d9fe9503a12bf
admin <mmd83@63.ru>
2023-05-23 22:21:02 +0400

    Entities

--- src/main/java/ru/skypro/hibernate/service/Application.java
+++ src/main/java/ru/skypro/hibernate/service/Application.java
@@ -1,5 +1,7 @@
 package ru.skypro.hibernate.service;
 
+import ru.skypro.hibernate.model.Employee;
+
 public class Application {
     public static void main(String[] args) {
         EmployeeDAO employeeDao = new EmployeeDaoImpl();
@@ -8,10 +10,10 @@ public class Application {
         System.out.println(((EmployeeDaoImpl) employeeDao).getAllEmployee());
         System.out.println(((CityDaoImpl) cityDao).getAllCities());
 
-//        employeeDao.addEmployee(new Employee("Ivan", "Ivanov", "male", 59, new City("Moscow")));
+        employeeDao.addEmployee(new Employee("Ivan", "Ivanov", "male", 59, cityDao.getCityById(1L)));
 //        cityDao.addCity(new City("SomeCity"));
 
-//        employeeDao.updateEmployee(11L, new Employee(11L, "Ivan", "Ivanov", "male", 47, new City("Kazan")));
+        employeeDao.updateEmployee(11L, new Employee(11L, "Ivan", "Ivanov", "male", 47, cityDao.getCityById(4L)));
 
         cityDao.deleteCity(16L);
         employeeDao.deleteEmployee(14L);
